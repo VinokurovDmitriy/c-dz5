f66e209daf81a7423b9d258fd6294a5ceeda04cd
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-04-24 14:33:38 +0300

    add task36

---
+++ task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными  числами.
+//найдите сумму элементов массива стоящих на не четных позициях
+int[] array = new int[10];
+void fillArrayRandom(int[] array){
+    for(int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void printArray(int[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int getEvenSum(int[] array){
+    int result = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(i % 2 != 0){
+            result += array[i];
+        }
+    }
+    return result;
+}
+
+fillArrayRandom(array);
+printArray(array);
+Console.WriteLine("Сумма элементов массива стоящих на не четных позициях равна " + getEvenSum(array));
